1838f1b25ecb44b2f5ecf0e9b1c38f4073776b7c
Mityal999 <litvinenko-mitya@yandex.ru>
2022-11-20 22:54:32 +0700

    Начал пилить механику замаха врагов

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Enemy : FragileEntity
 {
+
     [SerializeField] private float scoreForKill;
 
+    private void OnPlayerEnteredHitZone()
+    {
+        //StartCoroutine()
+    }
+
+    private IEnumerator BeforeHitProcess()
+    {
+        yield return new WaitForSeconds(1f);
+    }
 
     protected override void Die()
     {        
