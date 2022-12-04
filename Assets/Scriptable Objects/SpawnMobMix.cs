a0aa36fd66dac52ad68ed054954e83dc64b4d8eb
Mityal999 <litvinenko-mitya@yandex.ru>
2022-12-04 18:09:20 +0700

    SpawnSystem: скриптабл облджекты моб миксов

--- Assets/Scriptable Objects/SpawnMobMix.cs
+++ Assets/Scriptable Objects/SpawnMobMix.cs
@@ -1,48 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class UnitySerializedDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+public enum MobEnum
 {
-	[SerializeField, HideInInspector]
-	private List<TKey> keyData = new List<TKey>();
-	
-	[SerializeField, HideInInspector]
-	private List<TValue> valueData = new List<TValue>();
-
-    void ISerializationCallbackReceiver.OnAfterDeserialize()
-    {
-		this.Clear();
-		for (int i = 0; i < this.keyData.Count && i < this.valueData.Count; i++)
-		{
-			this[this.keyData[i]] = this.valueData[i];
-		}
-    }
-
-    void ISerializationCallbackReceiver.OnBeforeSerialize()
-    {
-		this.keyData.Clear();
-		this.valueData.Clear();
-
-		foreach (var item in this)
-		{
-			this.keyData.Add(item.Key);
-			this.valueData.Add(item.Value);
-		}
-    }
+	Zombie,
+	Head,
+	FatZombie,
+	FatHead
 }
 
-[CreateAssetMenu(fileName = "SpawnMobMix", menuName = "Platform-game/Assets/Scriptable Objects", order = 0)]
+[CreateAssetMenu(fileName = "SpawnMobMix", menuName = "Spawn/SpawnMobMix", order = 0)]
 public class SpawnMobMix : ScriptableObject
 {
-    public enum MobEnum
-    {
-        Zombie,
-        Head,
-        FatZombie,
-        FatHead
-    }
-    
-    public UnitySerializedDictionary<MobEnum,int> dict;
+    public MobEnum[] mobNames;
+    public int[] mobOdds;
 
 }
